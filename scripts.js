c3532594a41e6e1c702e09605052a6689574d722
Ben Easterwood <ben.easterwood@gmail.com>
2018-06-16 23:23:55 -0500

    squares generated

--- scripts.js
+++ scripts.js
@@ -3,4 +3,19 @@ document.addEventListener('DOMContentLoaded',function(){
     var btntext=document.createTextNode('Add Square');
     document.body.appendChild(button);
     button.appendChild(btntext);
+    var container=document.createElement('div');
+    document.body.appendChild(container);
+    container.className='container';
+    var sqrid=0;
+    function addsquare() {
+        var sqr=document.createElement('div')
+        container.appendChild(sqr);
+        sqr.className='square'
+        sqr.id=sqrid
+        sqr.style.backgroundColor="black"
+    }
+    button.addEventListener("click", function(){
+        addsquare();
+        sqrid++;
+    });
 })
