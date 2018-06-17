e5ffaaea03088724bbc695213cea6a33651dd152
Ben Easterwood <ben.easterwood@gmail.com>
2018-06-16 19:42:03 -0500

    initial upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',function(){
+    var button=document.createElement('button');
+    var btntext=document.createTextNode('Add Square');
+    document.body.appendChild(button);
+    button.appendChild(btntext);
+})
