9d8f9633912c4a5b196086b2767e7bfb4bb5873a
Ben Easterwood <ben.easterwood@gmail.com>
2018-06-17 23:48:26 -0500

    id dispay added

--- scripts.js
+++ scripts.js
@@ -7,12 +7,32 @@ document.addEventListener('DOMContentLoaded',function(){
     document.body.appendChild(container);
     container.className='container';
     var sqrid=0;
+    function randcolor() {
+        var col='#'+Math.floor(Math.random()*16777215).toString(16)
+        return col
+    }
     function addsquare() {
         var sqr=document.createElement('div')
         container.appendChild(sqr);
-        sqr.className='square'
-        sqr.id=sqrid
-        sqr.style.backgroundColor="black"
+        sqr.className='square';
+        sqr.id=sqrid;
+        sqr.style.backgroundColor="black";
+        var iddis=document.createElement("h3")
+        var sid=document.createTextNode(String(sqrid));
+        iddis.appendChild(sid);
+        sqr.appendChild(iddis);
+        iddis.style.color="white"
+        iddis.style.visibility="hidden"
+    sqr.addEventListener("click",function() {
+        var colr=randcolor();
+        sqr.style.backgroundColor=colr;
+    });
+    sqr.addEventListener('mouseover', function() {
+        iddis.style.visibility="visible"
+    })
+    sqr.addEventListener('mouseout', function() {
+        iddis.style.visibility="hidden"
+    })
     }
     button.addEventListener("click", function(){
         addsquare();
