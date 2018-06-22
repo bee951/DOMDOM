c81c3e103911e37fb682cc9097276ab3a650ad8c
Ben Easterwood <ben.easterwood@gmail.com>
2018-06-21 23:13:54 -0500

    completed project

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded',function(){
     document.body.appendChild(container);
     container.className='container';
     var sqrid=0;
+    var idary=[];
     function randcolor() {
         var col='#'+Math.floor(Math.random()*16777215).toString(16)
         return col
@@ -23,15 +24,58 @@ document.addEventListener('DOMContentLoaded',function(){
         sqr.appendChild(iddis);
         iddis.style.color="white"
         iddis.style.visibility="hidden"
+        iddis.style.margin="0"
+        iddis.style.padding="1.57em"
+        iddis.style.justifyContent="center"
+        idary.push(sqrid);
+        console.log(idary)
     sqr.addEventListener("click",function() {
         var colr=randcolor();
-        sqr.style.backgroundColor=colr;
+        sqr.style.backgroundColor=colr;  
     });
     sqr.addEventListener('mouseover', function() {
         iddis.style.visibility="visible"
     })
     sqr.addEventListener('mouseout', function() {
         iddis.style.visibility="hidden"
+    })
+    sqr.addEventListener("dblclick",function(){
+        var itemid=this.id
+        var sqra=idary.findIndex(function findidnd(itid) {
+            return itid==itemid;
+        })
+        console.log(itemid)
+        console.log(sqra)
+        if (sqra==-1) {
+            alert("Unable to find square to remove")
+        } else {
+        if (itemid%2==0) {
+            var impst=sqra+1
+            if (idary.length==impst||idary.length==1) {
+                alert("No square following this one.")
+            } else {
+            console.log(impst)
+            var sqarid=idary[impst]
+            console.log(sqarid)
+            var sqrp=document.getElementById(sqarid);
+            container.removeChild(sqrp)
+            idary.splice(impst,1)    
+            }
+        } else {
+            var inmst=sqra-1
+            if (inmst>idary.length||inmst==-1) {
+                alert("No square preceding this one.")
+            } else {
+            console.log(inmst)
+            var sqaridm=idary[inmst]
+            console.log(sqaridm) 
+            var sqrm=document.getElementById(sqaridm);
+            container.removeChild(sqrm)
+            idary.splice(inmst,1)    
+            }
+            
+        }}
+        console.log(idary)
     })
     }
     button.addEventListener("click", function(){
